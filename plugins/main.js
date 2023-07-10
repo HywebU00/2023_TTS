9200386d1964846b9e737340d102c84dab10b4f1
Tseng <tseng7510@gmail.com>
2023-07-10 09:51:43 +0800

    修改調整

--- plugins/main.js
+++ plugins/main.js
@@ -90,6 +90,91 @@ new WOW({
   pic.forEach((item) => observer.observe(item));
 })();
 
+$(function () {
+  $('.mainMenu li').has('ul').addClass('nextLv').append('<span class="toggleBtn"></span>');
+
+  $('.nextLv').hover(
+    function () {
+      if ($(window).outerWidth() > 1000) {
+        $(this).children('ul').stop().off().slideDown('fast');
+        $(this).addClass('active');
+      }
+    },
+    function () {
+      if ($(window).outerWidth() > 1000) {
+        $(this).children('ul').stop().off().slideUp('fast');
+        $(this).removeClass('active');
+      }
+    }
+  );
+  $('.nextLv a').on('click', function () {
+    if ($(window).outerWidth() <= 1000 && $(this).attr('href') === '#') {
+      $(this).siblings('.toggleBtn').trigger('click');
+    }
+  });
+  $('.nextLv .toggleBtn').on('click', function () {
+    $(this).parents('li').siblings('li').not($(this).parents('li')).children('ul').slideUp('fast');
+    $(this).siblings('ul').stop().slideToggle('fast');
+    $(this).parent('li').toggleClass('active');
+  });
+});
+
+$(window).on('load resize', function () {
+  $('.mainMenu li').removeClass('active');
+  $('.mainMenu ul').attr('style', null);
+});
+
+// let slideWrapper = $('.bigPic .listBox');
+// let lazyCounter = 0;
+
+// function postMessageToPlayer(player, command) {
+//   if (player == null || command == null) return;
+//   player.contentWindow.postMessage(JSON.stringify(command), '*');
+// }
+
+// function playPauseVideo(slick, control) {
+//   let currentSlide = slick.find('.slick-current .item');
+//   let slideType = currentSlide.attr('class').split(' ')[1];
+//   let player = currentSlide.find('iframe').get(0);
+//   let startTime = currentSlide.data('video-start');
+
+//   if (slideType === 'youtube') {
+//     switch (control) {
+//       case 'play':
+//         postMessageToPlayer(player, {
+//           event: 'command',
+//           func: 'mute',
+//         });
+//         postMessageToPlayer(player, {
+//           event: 'command',
+//           func: 'playVideo',
+//         });
+//         break;
+//       case 'pause':
+//         postMessageToPlayer(player, {
+//           event: 'command',
+//           func: 'pauseVideo',
+//         });
+//         break;
+//     }
+//   }
+// }
+
+// slideWrapper.on('init', function (slick) {
+//   slick = $(slick.currentTarget);
+//   setTimeout(function () {
+//     playPauseVideo(slick, 'play');
+//   }, 1000);
+// });
+// slideWrapper.on('beforeChange', function (event, slick) {
+//   slick = $(slick.$slider);
+//   playPauseVideo(slick, 'pause');
+// });
+// slideWrapper.on('afterChange', function (event, slick) {
+//   slick = $(slick.$slider);
+//   playPauseVideo(slick, 'play');
+// });
+
 (function () {
   const demoContent = `
     <div class="demoBox">
@@ -131,37 +216,3 @@ new WOW({
   const nowActive = document.querySelector(`.${String(cookieCss)}`);
   nowActive?.classList.add('active');
 })();
-
-$(function () {
-  $('.mainMenu li').has('ul').addClass('nextLv').append('<span class="toggleBtn"></span>');
-
-  $('.nextLv').hover(
-    function () {
-      if ($(window).outerWidth() > 1000) {
-        $(this).children('ul').stop().off().slideDown('fast');
-        $(this).addClass('active');
-      }
-    },
-    function () {
-      if ($(window).outerWidth() > 1000) {
-        $(this).children('ul').stop().off().slideUp('fast');
-        $(this).removeClass('active');
-      }
-    }
-  );
-  $('.nextLv a').on('click', function () {
-    if ($(window).outerWidth() <= 1000 && $(this).attr('href') === '#') {
-      $(this).siblings('.toggleBtn').trigger('click');
-    }
-  });
-  $('.nextLv .toggleBtn').on('click', function () {
-    $(this).parents('li').siblings('li').not($(this).parents('li')).children('ul').slideUp('fast');
-    $(this).siblings('ul').stop().slideToggle('fast');
-    $(this).parent('li').toggleClass('active');
-  });
-});
-
-$(window).on('load resize', function () {
-  $('.mainMenu li').removeClass('active');
-  $('.mainMenu ul').attr('style', null);
-});
