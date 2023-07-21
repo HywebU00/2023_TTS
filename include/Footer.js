3a1c6ebb9d2b803c8b222944ecc2c1eb451511ee
Tseng <tseng7510@gmail.com>
2023-07-21 10:35:11 +0800

    更新

--- include/Footer.js
+++ include/Footer.js
@@ -1,21 +1,113 @@
-const FooterContent = `<section class="section col-12 full">
-          <div class="container">
-            <div class="col">
-              <div class="footerBox">
-                <div class="logo"><a href="#"></a></div>
-                <nav>
-                  <ul>
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="#">ABOUT TAITRA</a></li>
-                    <li><a href="#">TAITRA OVERSEA</a></li>
-                  </ul>
-                </nav>
-                <div class="copyright">All Rights Reserved by TAITRA 2023</div>
-              </div>
-            </div>
-          </div>
-          <button class="goTop">
-            <svg><use xlink:href="#arrow3" /></svg>
+const FooterContent = `
+<button class="goTop" style="opacity: 1;">
+            <svg><use xlink:href="#arrow3"></use></svg>
           </button>
-        </section>`;
+<section class="fatfooter">
+            <div class="container">
+                <button type="button" name="收合" class="btn btn-fatfooter">收合</button>
+                <nav>
+                    <ul>
+                        <li><a href="#">展覽介紹</a>
+                            <ul>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">展覽介紹</a></li>
+                        <li><a href="#">主題活動</a>
+                            <ul>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層有連結</a>
+                                    <ul>
+                                        <li><a href="http://www.google.com">第三層選單有下層</a>
+                                            <ul>
+                                                <li><a href="#">第四層選單</a></li>
+                                                <li><a href="#">第四層選單</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="http://www.google.com">第三層選單有連結</a></li>
+                                        <li><a href="#">第三層選單</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">第二層選單</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">影音專區</a>
+                            <ul>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">參展登記</a>
+                            <ul>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">廠商總覽</a>
+                            <ul>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">檔案下載</a>
+                            <ul>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                                <li><a href="#">第二層選單</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+        <footer class="has_bottom">
+            <div class="container">
+                <!-- footer資訊模組 -->
+                <div class="footer_info">
+                    <ul class="footer_share">
+                        <li><a href="#"><img data-src="images/icon/icon_facebook.svg" alt="" class="lazy loaded" src="images/icon/icon_facebook.svg" data-was-processed="true"></a></li>
+                        <li><a href="#"><img data-src="images/icon/icon_twitter.svg" alt="" class="lazy loaded" src="images/icon/icon_twitter.svg" data-was-processed="true"></a></li>
+                        <li><a href="#"><img data-src="images/icon/icon_youtube.svg" alt="" class="lazy loaded" src="images/icon/icon_youtube.svg" data-was-processed="true"></a></li>
+                        <li><a href="#"><img data-src="images/icon/icon_instagram.svg" alt="" class="lazy loaded" src="images/icon/icon_instagram.svg" data-was-processed="true"></a></li>
+                        <li><a href="#"><img data-src="images/icon/icon_pinterest.svg" alt="" class="lazy loaded" src="images/icon/icon_pinterest.svg" data-was-processed="true"></a></li>
+                    </ul>
+                    <ul class="footer_link">
+                        <li><a href="#">服務條款和隱私權政策內容</a></li>
+                        <li><a href="#">免責聲明</a></li>
+                        <li><a href="#">國際貿易局廣告</a></li>
+                        <li><a href="#">聯絡我們</a></li>
+                    </ul>
+                    <p class="Copyright">中華民國對外貿易發展協會</p>
+                    <p>本網站主辦單位<a href="#"><img src="images/icon/fp0000149.png" alt="">經濟部國際貿易局</a> 執行單位<a href="#"><img src="images/icon/fp0000150.png" alt="">中華民國對外貿易發展協會</a></p>
+                </div>
+                <!-- APP下載模組 -->
+                <div class="footer_app">
+                    <h3>下載APP</h3>
+                    <div class="app_download">
+                        <a target="_blank" href="#" title="另開視窗">
+                            <img data-src="images/google_play.png" alt="" class="lazy loaded" src="images/google_play.png" data-was-processed="true">
+                        </a>
+                        <a target="_blank" href="#" title="另開視窗">
+                            <img data-src="images/app_store.png" alt="" class="lazy loaded" src="images/app_store.png" data-was-processed="true">
+                        </a>
+                    </div>
+                    <div class="qr_code">
+                        <a target="_blank" href="#" title="另開視窗">
+                            <img data-src="images/qrcode.jpg" alt="" class="lazy loaded" src="images/qrcode.jpg" data-was-processed="true">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>`;
 document.write(FooterContent);
--- plugins/main.js
+++ plugins/main.js
@@ -142,6 +142,24 @@ $(window).on('load resize', function () {
   $('.mainMenu ul').attr('style', null);
 });
 
+$(function () {
+  $('.btn-fatfooter').click(function (e) {
+    $(this)
+      .parent('.container')
+      .find('nav>ul>li>ul')
+      .stop(true, true)
+      .slideToggle(function () {
+        if ($(this).is(':visible')) {
+          $('.btn-fatfooter').html('收合/CLOSE');
+          $('.btn-fatfooter').attr('name', '收合選單/CLOSE');
+        } else {
+          $('.btn-fatfooter').html('展開/OPEN');
+          $('.btn-fatfooter').attr('name', '展開選單/OPEN');
+        }
+      });
+    $(this).stop(true, true).toggleClass('close');
+  });
+});
 (function () {
   let goTopBtn = document.querySelector('.goTop');
 
